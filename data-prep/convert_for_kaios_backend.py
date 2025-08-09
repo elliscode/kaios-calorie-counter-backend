fb9146b96566832e8d70407d54b7ad04f063841e
elliscode <45629640+elliscode@users.noreply.github.com>
2025-08-08 22:03:14 -0400

    working almost 100% of the way there

--- data-prep/convert_for_kaios_backend.py
+++ data-prep/convert_for_kaios_backend.py
@@ -1,27 +1,65 @@
 import re
 import json
+
 import json_stream
 from json_stream_to_standard_types import to_standard_types
 
 # fat, carbs, protein, caffeine, alcohol
 stupid_foods = [
     "NOT A DESCRIPTIVE ITEM".lower(),
+    "OREO MINI BISCUITS VANILLA 150 GR".lower(),
+]
+stupid_upcs = [
+    '713733769433',
 ]
 stupid_servings = [
-    'Guideline amount per fl oz of beverage'.lower(),
-    'Quantity not specified'.lower(),
-    'Guideline amount per cup of hot cereal'.lower(),
-    'N/A'.lower(),
-    'GRM'.lower(),
+    re.compile('Guideline amount per fl oz of beverage', re.IGNORECASE),
+    re.compile('Quantity not specified', re.IGNORECASE),
+    re.compile('Guideline amount per cup of hot cereal', re.IGNORECASE),
+    re.compile('N/A', re.IGNORECASE),
+    re.compile('GRM', re.IGNORECASE),
+]
+servings_fix_these_phrases = [
+    {'find': re.compile(r'\bz\b$', re.IGNORECASE), 'replace': 'oz'},
+    {'find': re.compile(r'1 % COOKIES', re.IGNORECASE), 'replace': '1 cookie'},
+    {'find': re.compile(r'2 ~3TSP', re.IGNORECASE), 'replace': '3 tsp'},
+    {'find': re.compile(r'2\.1 /2"" SLICES', re.IGNORECASE), 'replace': '2 half-inch slices'},
+    {'find': re.compile(r'4/0.48 Bites', re.IGNORECASE), 'replace': '4 bites'},
+
+    {'find': re.compile(r'\d+\s*[\'\"]*\s*x\s*\d+\s*[\'\"]*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'[(\[][^)]+[)\]]', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'(\d+)th\b', re.IGNORECASE), 'replace': r'\1'},
+    {'find': re.compile(r'about\s*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'~\s*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'Null\s*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'About\s*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'Approx\.*\s*', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'Seasons 2 tacos', re.IGNORECASE), 'replace': '2 tsp. mix'},
+    {'find': re.compile(r'onz', re.IGNORECASE), 'replace': 'oz'},
+    {'find': re.compile(r'oza', re.IGNORECASE), 'replace': 'fl oz'},
+    {'find': re.compile(r'\[([^]]+)]', re.IGNORECASE), 'replace': ''},
+    {'find': re.compile(r'\bdia[.]*\b', re.IGNORECASE), 'replace': 'diameter'},
+    {'find': re.compile(r'"+', re.IGNORECASE), 'replace': '"'},
+    {'find': re.compile(r'^1 \[$', re.IGNORECASE), 'replace': '1 packet'},
+    {'find': re.compile(r'\u00BC', re.IGNORECASE), 'replace': '1/4'},
+    {'find': re.compile(r'[^\u0020-\u007E]', re.IGNORECASE), 'replace': ''},
+
 ]
 stupid_brands = [
     "Not A Branded Item".lower(),
     "N/A".lower(),
 ]
+needs_more_info_brands = [
+    'Farmland'.lower()
+]
 brand_fixes = {
     "Psst...": "P$$t...",
     "Psst": "P$$t...",
     "Kellogg Company Us": "Kellogg Company",
+}
+serving_fixes = {
+    "Piecesq": "pieces",
+    "Stick | Null": "stick",
 }
 macros = {
     'Total lipid (fat)': 'fat',
@@ -42,20 +80,60 @@ macros = {
     'not-present-2': 'addedSugar',
 }
 
-leading_numbers_regex = re.compile(r"^(\d+\.?\d*)\s+([^(]+)")
+leading_range_regex = re.compile(r"^(\d+)\s*-\s*(\d+)\b\s*([^(]*)")
+leading_irregular_fraction_regex = re.compile(r"^(\d+)[\s\-]+(\d+[\\/]\d+)\s+([^(]*)")
+leading_fraction_regex = re.compile(r"^(\d+\s*[\\/]\s*\d+)\s*([^(]*)")
+leading_numbers_regex = re.compile(r"^(\d*[,.]?\d+)\b\s*([^(]*)")
 apostrophe_s = re.compile(r"'S")
 whitespace = re.compile(r"\s+")
 acai_berry = re.compile(r"AA BERRY", re.IGNORECASE)
 two_as = re.compile(r"\ba{2}\b", re.IGNORECASE)
 
 def my_titlecase(input_string):
-    # AA BERRY
     output = input_string
     output = re.sub(apostrophe_s, "'s", output.title())
     output = re.sub(whitespace, " ", output)
     output = re.sub(acai_berry, "Acai Berry", output)
     output = re.sub(two_as, "AA", output)
     return output
+
+def parse_portion(q, p):
+    q, p = inner_parse_portion(q, p)
+    if not p:
+        p = 'count'
+    return q, p.strip("- ()|'\",.+/~}{][;")
+
+def inner_parse_portion(q, p):
+    for bad_thing in servings_fix_these_phrases:
+        p = bad_thing['find'].sub(bad_thing['replace'], p)
+    output = leading_irregular_fraction_regex.match(p)
+    if output:
+        return float(output.group(1)) + eval(output.group(2).replace('\\', '/')), output.group(3).strip()
+    output = leading_range_regex.match(p)
+    if output:
+        return (float(output.group(1)) + float(output.group(2))) / 2.0, output.group(3).strip()
+    output = leading_fraction_regex.match(p)
+    if output:
+        return eval(output.group(1).replace('\\', '/')), output.group(2).strip()
+    output = leading_numbers_regex.match(p)
+    if output:
+        return float(output.group(1).replace(',', '.')), output.group(2).strip()
+    return q, p
+
+def write_servings(input_servings: set):
+    with open("servings.jsonl", "w") as servings_file:
+        for unique_serving in sorted(input_servings):
+            servings_file.write(unique_serving + '\n')
+
+def is_portion_stupid(input_text):
+    for stupid_serving in stupid_servings:
+        if stupid_serving.findall(input_text):
+            return True
+    return False
+
+
+unique_servings = set()
+count = 0
 
 with open("output_my_titlecase.jsonl", "w") as output_file:
     with open("../data/surveyDownload.json", 'r') as file:
@@ -69,13 +147,10 @@ with open("output_my_titlecase.jsonl", "w") as output_file:
             servings = []
             for portion in item['foodPortions']:
                 portion_name = portion['portionDescription']
-                if portion_name in stupid_servings:
+                if is_portion_stupid(portion_name):
                     continue
                 quantity = 1
-                result = leading_numbers_regex.match(portion_name)
-                if result:
-                    quantity = float(result.group(1))
-                    portion_name = result.group(2).strip()
+                quantity, portion_name = parse_portion(quantity, portion_name)
                 portion_grams = portion['gramWeight']
                 ratio = portion_grams / 100.0
                 serving = { 'name': portion_name, 'quantity': quantity }
@@ -85,12 +160,23 @@ with open("output_my_titlecase.jsonl", "w") as output_file:
                     serving[key] = round(serving_100g[key] * ratio, 2)
                 servings.append(serving)
             servings.append(serving_100g)
+
+            for serving in servings:
+                unique_servings.add(serving['name'])
+
             json_line = json.dumps({'name': my_titlecase(item['description']), 'servings': servings})
             output_file.write(json_line + "\n")
+            count = count + 1
+            if count > 10000:
+                count = 0
+                write_servings(unique_servings)
     with open("../data/FoodData_Central_branded_food_json_2025-04-24.json", 'r') as file:
         raw_data = json_stream.load(file)
         for item_stream in raw_data["BrandedFoods"]:
             item = to_standard_types(item_stream)
+            upc = item['gtinUpc']
+            if upc in stupid_upcs:
+                continue
             if item['description'].lower() in stupid_foods:
                 continue
             skip = False
@@ -115,24 +201,16 @@ with open("output_my_titlecase.jsonl", "w") as output_file:
             if 'householdServingFullText' in item:
                 portion_name = my_titlecase(item['householdServingFullText'])
                 quantity = 1
-                result = leading_numbers_regex.match(portion_name)
-                if result:
-                    quantity = float(result.group(1))
-                    portion_name = result.group(2).strip()
-
-                if portion_name.lower() not in stupid_servings:
-                    if portion_name.lower() == 'onz':
-                        portion_name = 'oz'
-                    elif portion_name.lower() == 'oza':
-                        portion_name = 'fl oz'
-
+                quantity, portion_name = parse_portion(quantity, portion_name)
+                if not is_portion_stupid(portion_name):
                     serving = { 'name': portion_name, 'quantity': quantity, **household_serving }
                     servings.append(serving)
 
             if 'servingSizeUnit' in item:
                 portion_name = item['servingSizeUnit']
-                if portion_name.lower() not in stupid_servings:
+                if not is_portion_stupid(portion_name):
                     quantity = item['servingSize']
+                    quantity, portion_name = parse_portion(quantity, portion_name)
 
                     serving = { 'name': portion_name, 'quantity': quantity, **household_serving }
                     servings.append(serving)
@@ -144,12 +222,17 @@ with open("output_my_titlecase.jsonl", "w") as output_file:
             if not any_grams:
                 servings.append(serving_100g)
 
+            for serving in servings:
+                unique_servings.add(serving['name'])
+
             name = item['description']
             if 'subbrandName' in item and item['subbrandName'] and item['subbrandName'].lower() not in stupid_brands:
                 if item['subbrandName'].lower() not in name.lower():
                     name = item['subbrandName'] + " " + name
                 if item['brandName'].lower() not in name.lower() and item['brandName'].lower() not in stupid_brands:
                     name = item['brandName'] + " " + name
+            elif 'brandName' in item and item['brandName'].lower() in needs_more_info_brands:
+                name = item['brandOwner'] + " " + item['brandName'] + " " + name
             elif 'brandName' in item and item['brandName'] and item['brandName'].lower() not in stupid_brands:
                 if item['brandName'].lower() not in name.lower():
                     name = item['brandName'] + " " + name
@@ -160,5 +243,11 @@ with open("output_my_titlecase.jsonl", "w") as output_file:
             for key, value in brand_fixes.items():
                 if key in name:
                     name = name.replace(key, value)
-            json_line = json.dumps({'name': name, 'servings': servings})
+            json_line = json.dumps({'name': name, 'servings': servings, 'upc': upc})
             output_file.write(json_line + "\n")
+            count = count + 1
+            if count > 10000:
+                write_servings(unique_servings)
+                count = 0
+
+write_servings(unique_servings)
--- data-prep/test_parsing.py
+++ data-prep/test_parsing.py
@@ -1,15 +1,16 @@
 import re
-from titlecase import titlecase
 
 leading_numbers_regex = re.compile(r"^(\d+\.?\d*)\s+([^(]+)")
 print(leading_numbers_regex.match("1 tablespoon"))
 result = leading_numbers_regex.match("1.25 slice (4-3/8\" dia x 1/10\" thick) (1 oz)")
 print(result.group(1), result.group(2).strip())
 print("WELCH'S, CONCORD GRAPE SPREAD, GRAPE".title())
-print(titlecase("WELCH'S, CONCORD GRAPE SPREAD, GRAPE"))
-print(titlecase("DIGIORNO DIGIORNO Croissant Crust Pepperoni 6.5in 10x9oz Carton"))
+# print(titlecase("WELCH'S, CONCORD GRAPE SPREAD, GRAPE"))
+# print(titlecase("DIGIORNO DIGIORNO Croissant Crust Pepperoni 6.5in 10x9oz Carton"))
 
 print('testy' in 'testicle')
 
-print(titlecase('GENERAL MILLS SALES INC. Food Should Taste Good Guacamole Tortilla Chips'))
+# print(titlecase('GENERAL MILLS SALES INC. Food Should Taste Good Guacamole Tortilla Chips'))
 print(re.sub(re.compile(r"a{2}", re.IGNORECASE), "AA", "Grade Aa Eggs"))
+
+print(re.sub(re.compile(r'\d+\s*[\'\"]*\s*x\s*\d+\s*[\'\"]*', re.IGNORECASE), '', '1 \'\' X 4\'\' PIECE'))
